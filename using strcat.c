3e1b29e6b65b7c45bc4ad087c5f54150ccb2cf90
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-01-16 23:44:52 +0530

    add new

---
+++ using strcat.c
@@ -0,0 +1,19 @@
+//bey string merge thy
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[100], b[100];
+
+    printf("Enter first string: ");
+    gets(a);
+
+    printf("Enter second string: ");
+    gets(b);
+
+    strcat(a, b);    // a + b
+
+    printf("Concatenated string = %s", a);
+
+    return 0;
+}
